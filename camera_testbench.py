83584f81d9ade59310e4f373cd4e3ca570086d44
Sophie Li <sophie@Sophies-MBP.home>
2015-04-04 16:52:46 -0400

    inital workflow plan

--- camera_testbench.py
+++ camera_testbench.py
@@ -23,22 +23,55 @@ Python / C is fine for the language used.
 		Error handling should report the error and then abort the program.
 
 """
+
+"""
+Ok, so workflow:
+Initalize camera (probably want to make this a class)
+
+Get initial focus, getImage with said focus
+
+Check brightness of image. If < 150, loop back to getImage, else print/log warning and loop back to getImage
+
+Calculate imageCenter of image, changing X, Y servo positions until center is w/2, h/2. These servo movements should be
+pretty large.
+
+Calculate imageOffset in loop. Go through X, Y, Z positions until all offsets are zero (Arduino + SEROVS here). 
+While in this loop. These servo movements should be smaller.
+
+Calculate sharpness of image. Take a bunch of images here in case the lighting conditions are changing. Shouldn't
+need to move servos nor calculate focusPostion
+
+Save sharpest image. End program.
+
+Notes:
+(Are we outputting errors to a .txt file? Might as well)
+getFocus needs to be called every time a servo is moved.
+Don't know how the servo control scheme works, just assume we're moving it by some scale
+Why would we need to use external code/apis when the assumed scripts do the things we need it to do?
+
+
+"""
 #!/usr/bin/env python
 
-from appleCamera import *
-from cameraProcessing import *
+#importing this way so it's clear which interface functions are coming from
+import appleCamera 
+import cameraProcessing
 
 if __name__ == "__main__":
 	print "hello"
-	integrationTimes = 10
-	cameraPort = 1
-	cameraHandle = initCamera(cameraPort)
+	integrationTimes = 10 #assuming we want to keep this constant, choosing random number for now
+	cameraPort = 1 #probably want to make this an argv input
+	cameraHandle = appleCamera.initCamera(cameraPort)
 	if cameraHandle == -1:
 		raise AssertionError('Camera could not initialize, check connections')
+
 	while True: #this is bad, fix this, loops forever
-		focusPostion = getFocus() #this auto focuses the image
-		image = getImage(integrationTimes, focusPostion)
-		brightness = getBrightness(image)
-		sharpness = getSharpness(image)
-		imageCenter = getCenter(image)
-		imageOffset = getOffset(image)
+		focusPostion = getFocus(cameraHandle) #focus will change every time to we move the servos
+		image = cameraProcessing.getImage(integrationTimes, focusPostion)
+		try:
+			brightness = cameraProcessing.getBrightness(image)
+		except:
+			raise AssertionError('Image brightness < 150, retake image')
+		sharpness = cameraProcessing.getSharpness(image)
+		imageCenter = cameraProcessing.getCenter(image)
+		imageOffset = cameraProcessing.getOffset(image)
